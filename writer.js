f2d8e95dfd1e75fedb8d04ef24e6f32af2cca807
Wiktor Maciążek <75098062+swapperDEV@users.noreply.github.com>
2021-01-31 18:01:52 +0100

    package fix

--- writer.js
+++ writer.js
@@ -3,3 +3,21 @@ area.value = localStorage.getItem("textSave");
 area.addEventListener('keyup', () => {
     localStorage.setItem("textSave", area.value)
 })
+
+
+
+
+
+
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('Github please change project language from CSS to JS');
+console.log('THANKS');
